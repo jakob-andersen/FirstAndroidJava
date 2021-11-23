f40b943152a63250fbe1392bea73a384cc8171af
Jakob Andersen <jakob.andersen@dvconsulting.dk>
2021-11-24 00:12:53 +0100

    after task 8 in https://developer.android.com/codelabs/build-your-first-android-app binding used instead of the "global" textView variable

--- app/src/main/java/com/example/firstandroidjava/FirstFragment.java
+++ app/src/main/java/com/example/firstandroidjava/FirstFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,16 +13,23 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.firstandroidjava.databinding.FragmentFirstBinding;
 
+
+
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    // Inflate the layout for this fragment
+
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        // Inflate the layout for this fragment
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
 
+        //return fragmentFirstLayout;
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -36,6 +45,26 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        binding.toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast myToast = Toast.makeText(getActivity(), R.string.toast_message, Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        binding.countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+            }
+        });
+
+
+
+
+
     }
 
     @Override
@@ -44,4 +73,14 @@ public class FirstFragment extends Fragment {
         binding = null;
     }
 
+    private void countMe(View view) {
+        // Get the value of the text view
+        String countString = binding.textviewFirst.getText().toString();
+        // Convert value to a number and increment it
+        Integer count = Integer.parseInt(countString);
+        count++;
+        // Display the new value in the text view.
+        binding.textviewFirst.setText(count.toString());
+    }
 }
+
